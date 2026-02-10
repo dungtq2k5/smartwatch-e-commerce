b32512f7b3e2de6e6626a7f0433c5146aa97c584
Dung Tran <dungtranquang2005@gmail.com>
2026-02-10 12:18:48 +0700

    fix(server/variation.middleware.ts): update 'catergory' param for 'isValidImgUrls' since the function has been changed

--- server/utils/middlewares/product/variation.middleware.ts
+++ server/utils/middlewares/product/variation.middleware.ts
@@ -174,7 +174,7 @@ export function verifyVariationInput(
           }
           if (
             isPresent(imageUrls) &&
-            !(await isValidImgUrls(imageUrls, "product"))
+            !(await isValidImgUrls(imageUrls, "product-image"))
           ) {
             errors.push("image URLs must be an array of valid image URLs.");
           }
@@ -334,7 +334,7 @@ export function verifyVariationInput(
           }
           if (
             isPresent(imageUrls) &&
-            !(await isValidImgUrls(imageUrls, "product"))
+            !(await isValidImgUrls(imageUrls, "product-image"))
           ) {
             errors.push("image URLs must be an array of valid image URLs.");
           }
@@ -489,11 +489,19 @@ export function verifyVariationInput(
             sortBy,
           } = req["sanitizedQuery"] || req.query;
 
-          if (limit !== undefined && !isValidNumString(limit)) {
-            errors.push("limit must be a valid number string.");
+          if (limit !== undefined) {
+            if (!isValidNumString(limit)) {
+              errors.push("limit must be a valid number string.");
+            } else if (Number(limit) <= 0) {
+              errors.push("limit must be greater than 0.");
+            }
           }
-          if (offset !== undefined && !isValidNumString(offset)) {
-            errors.push("offset must be a valid number string.");
+          if (offset !== undefined) {
+            if (!isValidNumString(offset)) {
+              errors.push("offset must be a valid number string.");
+            } else if (Number(offset) < 0) {
+              errors.push("offset must be greater than or equal to 0.");
+            }
           }
           if (
             searchTerm !== undefined &&
